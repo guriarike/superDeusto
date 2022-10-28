ff7a802777fe2a9ce069ea5b0b4091399cb78162
asierjauregi <asierjauregi@localhost>
2022-10-28 09:43:01 +0200

    UserTest hecho.

--- Test/es/deusto/prog3/g01/UserTest.java
+++ Test/es/deusto/prog3/g01/UserTest.java
@@ -5,10 +5,64 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class UserTest {
+	
+	private User user;
+	private String userName = "aaa";
+	private int id_usuario = 123;
+	private String userContraseña = "bbb" ;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetUserName() {
+		assertEquals(userName, user.getUserName());
 	}
-
+	
+	@Test
+	public void testSetUserName() {
+		String newUserName = "aaa";
+		
+		assertEquals(userName, user.getUserName(),"");
+		user.setUserName(newUserName);
+		assertEquals(newUserName, user.getUserName(), "");
+		
+	}
+	
+	
+	@Test
+	public void testGetId_usuario() {
+		assertEquals(id_usuario, user.getId_usuario());
+	}
+	
+	@Test
+	public void testSetId_usuario() {
+		int newId_usuario = 123;
+		
+		assertEquals(id_usuario, user.getId_usuario(), 0);
+		user.setId_usuario(newId_usuario);
+		assertEquals(newId_usuario, user.getId_usuario(), 0);
+		
+	}
+	
+	
+	@Test
+	public void testGetUserContraseña() {
+		assertEquals(userContraseña, user.getUserContraseña());
+	}
+	
+	@Test
+	public void testSetUserContraseña() {
+		String newUserContraseña = "bbb";
+		
+		assertEquals(userContraseña, user.getUserContraseña(),"");
+		user.setUserContraseña(newUserContraseña);
+		assertEquals(newUserContraseña, user.getUserContraseña(), "");
+		
+	}
+	
+	
+	@Test
+	public void testToString() {
+		String toString = "User [userName=" + userName + ", id_usuario=" + id_usuario + "]";
+		
+		assertEquals(toString, user.toString());
+	}
 }
