aa4a848a91f84962690ef05ac956657ae9c84e58
asierjauregi <asierjauregi@localhost>
2022-10-28 09:20:47 +0200

    PersonaTest hecho menos lo de LocalDateTime que no se como funciona.

--- Test/es/deusto/prog3/g01/PersonaTest.java
+++ Test/es/deusto/prog3/g01/PersonaTest.java
@@ -2,13 +2,75 @@ package es.deusto.prog3.g01;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.Test;
 
 public class PersonaTest {
+	
+	private Persona persona;
+	private String nombre = "AAAA";
+	private String apellido = "BBBB";
+	private LocalDateTime fechaNacimiento;
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetNombre() {
+		assertEquals(nombre, persona.getNombre());
 	}
-
+	
+	@Test
+	public void testSetNombre() {
+		String newNombre = "AAAA";
+		
+		assertEquals(nombre, persona.getNombre(),"");
+		persona.setNombre(newNombre);
+		assertEquals(newNombre, persona.getNombre(), "");
+		
+		
+	}
+	
+	
+	@Test
+	public void testGetApellido() {
+		assertEquals(apellido, persona.getApellido());
+	}
+	
+	@Test
+	public void testSetApellido() {
+		String newApellido = "BBBB";
+		
+		assertEquals(nombre, persona.getApellido(),"");
+		persona.setApellido(newApellido);
+		assertEquals(newApellido, persona.getApellido(), "");
+		
+		
+	}
+	
+	
+	@Test
+	public void testGetFechaNacimiento() {
+		assertEquals(fechaNacimiento, persona.getFechaNacimiento());
+	}
+	
+/*
+	@Test
+	public void testSetFechaNacimiento() {
+		LocalDateTime newFechaNacimiento = ; 
+		
+		assertEquals(fechaNacimiento, persona.getFechaNacimiento(),"");
+		persona.setFechaNacimiento();
+		assertEquals(newFechaNacimiento, persona.getFechaNacimiento(), "");
+		
+		
+	}
+	
+*/
+	
+	@Test
+	public void testToString() {
+		String toString = "Persona [nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + "]";
+		
+		assertEquals(toString, persona.toString());
+	}
 }
