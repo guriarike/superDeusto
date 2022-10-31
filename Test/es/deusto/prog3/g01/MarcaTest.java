ec147447c79bb24913a4776d805c7066648001bf
Jon Ander Gallastegui <gallastegi.jonander@opendeusto.es>
2022-10-31 20:37:34 +0100

    marcaTest

--- Test/es/deusto/prog3/g01/MarcaTest.java
+++ Test/es/deusto/prog3/g01/MarcaTest.java
@@ -2,13 +2,50 @@ package es.deusto.prog3.g01;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class MarcaTest {
+	
+	private Marca marca;
+	private String nombreMarca = "AAAA";
+	private int idMarca = 01;
+	private ArrayList<Marca> listaProductosMarca;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetNombreMarca() {
+		assertEquals(nombreMarca, marca.getNombreMarca());
 	}
+	
+	@Test
+	public void testSetNombre() {
+		String newNombreMarca = "AAAA";
+		
+		assertEquals(nombreMarca, marca.getNombreMarca(),"");
+		marca.setNombreMarca(newNombreMarca);
+		assertEquals(newNombreMarca, marca.getNombreMarca(), "");
+		
+		
+	}
+	
+	
+	@Test
+	public void testIdMarca() {
+		assertEquals(idMarca, marca.getIdMarca());
+	}
+	
+	@Test
+	public void testSetIdMarca() {
+		Integer newIdMarca = 02;
+		
+		assertEquals(idMarca, marca.getIdMarca(), 0);
+		marca.setIdMarca(newIdMarca);
+		assertEquals(newIdMarca, marca.getIdMarca(), 0);
+		
+		
+	}
+	
 
 }
