e61b338a6c363d7bdaab5e19fcf55cd451308676
asierjauregi <asierjauregi@localhost>
2022-11-03 15:45:12 +0100

    Comienzo de CompraTest

--- Test/es/deusto/prog3/g01/CompraTest.java
+++ Test/es/deusto/prog3/g01/CompraTest.java
@@ -2,13 +2,24 @@ package es.deusto.prog3.g01;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.Test;
 
 public class CompraTest {
+	
+	private Compra compra;
+	private int idCompra = 0;
+	private Date fechaCompra = null;
+	private double precioCompra = 0.0;
+	private ArrayList<Producto> listaProductosDeCompra;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		
+		
 	}
 
 }
