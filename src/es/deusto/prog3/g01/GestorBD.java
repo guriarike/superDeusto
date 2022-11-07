eae9e4e5b56dc92d774488c22f70ac6e395d6b93
guriarike <guriarike@opendeusto.es>
2022-11-07 16:50:15 +0100

    Arreglado el problema del driver

--- src/es/deusto/prog3/g01/GestorBD.java
+++ src/es/deusto/prog3/g01/GestorBD.java
@@ -1,8 +1,14 @@
 package es.deusto.prog3.g01;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class GestorBD {
 	protected static final String DRIVER_NAME = "org.sqlite.JDBC";
@@ -27,13 +33,28 @@ public class GestorBD {
 					+ " ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
 					+ " NAME TEXT NOT NULL,\n"
 					+ "PRECIO DOUBLE NOT NULL,\n"
-					+ "";
-								
+					+ ");\n";
+			String Seccion= "CREATE TABLE IF NOT EXISTS SECCION(\n"
+					   + "ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+					+ "NAMESECCION TEXT NOT NULL\n"
+					   +");\n";
+			String Marca= "CREATE TABEL IF NOT EXISTS MARCA(\n"
+					+ "ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+					+ "NAMEMARCA TEXT NOT NULL\n"
+					+ ");\n";
+			String Compra= "CREATE TABLE IF NOT EXISTS COMPRA(\n"
+					+ "ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+					+ "FECHA DATE NOT NULL,\n"
+					+ "PRECIO DOUBLE NOT NULL\n"
+					+ ");\n";
+			
 	        String sql = "CREATE TABLE IF NOT EXISTS USER (\n"
 	                   + " ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
 	                   + " NAME TEXT NOT NULL,\n"
 	                   + " PASSWORD TEXT NOT NULL\n"
-	                   + ");";
+	                   + ");\n"
+	                   + Producto+ Seccion + Marca + Compra;
+	        stmt.executeUpdate(sql);
 	        	        
 	        if (!stmt.execute(sql)) {
 	        	System.out.println("- Se ha creado la tabla Cliente");
@@ -42,5 +63,106 @@ public class GestorBD {
 			System.err.println(String.format("* Error al crear la BBDD: %s", ex.getMessage()));
 			ex.printStackTrace();			
 		}
+	}
+	public void borrarBBDD() {
+		//Se abre la conexión y se obtiene el Statement
+		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
+		     Statement stmt = con.createStatement()) {
+			
+	        String sql = "DROP TABLE IF EXISTS USER";
+			
+	        //Se ejecuta la sentencia de creación de la tabla Estudiantes
+	        if (!stmt.execute(sql)) {
+	        	System.out.println("- Se ha borrado la tabla Cliente");
+	        }
+		} catch (Exception ex) {
+			System.err.println(String.format("* Error al borrar la BBDD: %s", ex.getMessage()));
+			ex.printStackTrace();			
+		}
+		
+		
+	}
+	
+	
+	/*
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	//Metodos para leer y creer CSVs
+	public static ArrayList<User> crearListaDeUsuariosConCSV(String ruta) {
+		ArrayList<User> listaUsuarios = null;
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(ruta));
+			String strLine = "";
+			StringTokenizer st = null;
+			 listaUsuarios = new ArrayList<>();
+
+			while ((strLine = br.readLine()) != null) {
+				// break comma separated line using ","
+				st = new StringTokenizer(strLine, ",");
+				User u = new User();
+
+				u.setNombre(st.nextToken());
+
+				u.setApellido(st.nextToken());
+				String f = st.nextToken();
+
+				/*
+				 * aqui cambiamos el String del csv a LocalDateTimeDateTimeFormatter formatter =
+				 * DateTimeFormatter. ofPattern("yyyy-MM-dd"); LocalDateTime dateTime =
+				 * LocalDateTime. parse(f); u.setFechaNacimiento(dateTime);
+				 */
+
+				u.setUserName(st.nextToken());
+				u.setId_usuario(Integer.parseInt((String) st.nextElement()));
+				// añadimos el usuario de esta linea a la lista
+				listaUsuarios.add(u);
+
+			}
+			System.out.println("NOMBRES:");
+			for (User u : listaUsuarios) {
+				System.out.println("#" + u.getNombre());
+			}
+
+		} catch (Exception e) {
+			System.out.println("Exception while reading csv file: " + e);
+		}
+		return listaUsuarios;
+
+	}
+
+	public static void crearCSVusuarios(ArrayList<User> listaUsuarios, String rutaNuevoFichero) {
+		try {
+			//Creamos el escritor 
+			File file = new File(rutaNuevoFichero);
+			PrintWriter pr = new PrintWriter(file);
+			//Recorremos los usuarios uno por uno
+			for(User u:listaUsuarios) {
+				pr.println(""+u.getNombre()+";"+u.getApellido()+";"+u.getFechaNacimiento()+";"+u.getUserName()+";"+u.getId_usuario());
+				//comprobacion
+				System.out.println("#"+u.getNombre());
+			}
+			pr.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
 }
--- src/es/deusto/prog3/g01/Main.java
+++ src/es/deusto/prog3/g01/Main.java
@@ -15,99 +15,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
-		 * ArrayList<Producto> listacompra = new ArrayList<>(); String nombres[] =
-		 * {"guria","jaure","jonan", "gipu"}; Random random = new Random(); int index =
-		 * 0; for (int i= 0; i<30;i++) { int numero = random.nextInt(3); Producto
-		 * producto = new Producto(index, nombres[numero], null, 10);
-		 * listacompra.add(producto); }
-		 * 
-		 * Compra compra = new Compra(1, null, listacompra);
-		 * System.out.println(compra.toString());
-		 */
-		ArrayList<User> list = new ArrayList<>();
-		User u = new User();
-		u.setNombre("guria");
-		u.setApellido("rike");
-		u.setFechaNacimiento(null);
-		u.setUserName("guriarike");
-		u.setId_usuario(1);
-		User u1= new User();
-		u1.setNombre("asier");
-		u1.setApellido("jaure");
-		u1.setFechaNacimiento(null);
-		u1.setUserName("asierjaure");
-		u.setId_usuario(2);
-		list.add(u1);
-		list.add(u);
-		crearCSVusuarios(list, "data//nuevoCSV");
-	}
-
-	// METODOS PARA AÑADIR CSVs y TENER OBJETOS CREADOS POR NOSOTROS
-
-	public static ArrayList<User> crearListaDeUsuariosConCSV(String ruta) {
-		ArrayList<User> listaUsuarios = null;
-		try {
-			
-			BufferedReader br = new BufferedReader(new FileReader(ruta));
-			String strLine = "";
-			StringTokenizer st = null;
-			 listaUsuarios = new ArrayList<>();
-
-			while ((strLine = br.readLine()) != null) {
-				// break comma separated line using ","
-				st = new StringTokenizer(strLine, ",");
-				User u = new User();
-
-				u.setNombre(st.nextToken());
-
-				u.setApellido(st.nextToken());
-				String f = st.nextToken();
-
-				/*
-				 * aqui cambiamos el String del csv a LocalDateTimeDateTimeFormatter formatter =
-				 * DateTimeFormatter. ofPattern("yyyy-MM-dd"); LocalDateTime dateTime =
-				 * LocalDateTime. parse(f); u.setFechaNacimiento(dateTime);
-				 */
-
-				u.setUserName(st.nextToken());
-				u.setId_usuario(Integer.parseInt((String) st.nextElement()));
-				// añadimos el usuario de esta linea a la lista
-				listaUsuarios.add(u);
-
-			}
-			System.out.println("NOMBRES:");
-			for (User u : listaUsuarios) {
-				System.out.println("#" + u.getNombre());
-			}
-
-		} catch (Exception e) {
-			System.out.println("Exception while reading csv file: " + e);
-		}
-		return listaUsuarios;
+	GestorBD gestorBD = new GestorBD();		
+		
+		//CREATE DATABASE: Se crea la BBDD
+		gestorBD.crearBBDD();
+		
+		
 
 	}
-
-	/*
-	 * CREAR UN CSV CON UN ARRAYLIST DE USUARIOS QUE LE PASEMOS* POR EJEMPLO PARA
-	 * PASAR TODOS LOS USUARIOS DE LA BASE DE DATOS A UN CSV
-	 */
-	public static void crearCSVusuarios(ArrayList<User> listaUsuarios, String rutaNuevoFichero) {
-		try {
-			//Creamos el escritor 
-			File file = new File(rutaNuevoFichero);
-			PrintWriter pr = new PrintWriter(file);
-			//Recorremos los usuarios uno por uno
-			for(User u:listaUsuarios) {
-				pr.println(""+u.getNombre()+";"+u.getApellido()+";"+u.getFechaNacimiento()+";"+u.getUserName()+";"+u.getId_usuario());
-				//comprobacion
-				System.out.println("#"+u.getNombre());
-			}
-			pr.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-	}
-
 }
+	
